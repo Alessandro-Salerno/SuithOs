d8fef23a349ece9cb795cb6a772c77349e528990
AdawareDev <adawaredeveloper@gmail.com>
2022-05-14 23:34:25 +0000

    fix: ssb_files e main.c formattati

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -3,20 +3,22 @@
 #include <ssb_files.c>
 #include <elf.h>
 
-//TODO: Add support for posix-efi
+// TODO: Add support for posix-efi
 
-
-
-EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
 
 	InitializeLib(ImageHandle, SystemTable); // Initialize EFI library
 
-	EFI_FILE* kernel_file = ssb_load_file(NULL, L"kernel.elf", ImageHandle, SystemTable);
+	EFI_FILE *kernel_file = ssb_load_file(NULL, L"kernel.elf", ImageHandle, SystemTable);
 
-	if (kernel_file == NULL) {
+	if (kernel_file == NULL)
+	{
 		Print(L"Could not load kernel.elf\n");
 		return EFI_LOAD_ERROR;
-	}else{
+	}
+	else
+	{
 		Print(L"Loaded kernel.elf\n");
 	}
 
