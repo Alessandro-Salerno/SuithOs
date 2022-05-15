0c788d6346fca2dae32429a83c88ceba9836f405
AdawareDev <adawaredeveloper@gmail.com>
2022-05-15 08:51:15 +0000

    feat: verificare l'header del kernel

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -5,6 +5,15 @@
 
 // TODO: Add support for posix-efi
 
+int memcmp(const void* aptr, const void* bptr, long long n){
+	const unsigned char* a = aptr, *b = bptr;
+	for (long long i = 0; i < n; i++){
+		if (a[i] < b[i]) return -1;
+		else if (a[i] > b[i]) return 1;
+	}
+	return 0;
+}
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
 
@@ -21,6 +30,38 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 	{
 		Print(L"Loaded kernel.elf\n");
 	}
+
+	Elf64_Ehdr header;
+	{
+		UINTN size;
+		EFI_FILE_INFO file_info;
+		
+		kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &size, NULL);
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, size, (void **)&file_info);
+		kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &size, (void**)&file_info);
+
+		UINTN _size = sizeof(header);
+		kernel_file->Read(kernel_file, &_size, &header);
+
+
+	}
+
+	if (
+		memcmp(&header.e_ident[EI_MAG0], ELFMAG, SELFMAG) != 0 ||
+		header.e_ident[EI_CLASS] != ELFCLASS64 ||
+		header.e_ident[EI_DATA] != ELFDATA2LSB ||
+		header.e_type != ET_EXEC ||
+		header.e_machine != EM_X86_64 ||
+		header.e_version != EV_CURRENT
+	)
+	{
+		Print(L"kernel format is bad\r\n");
+	}
+	else
+	{
+		Print(L"kernel header successfully verified\r\n");
+	}
+
 
 	return EFI_SUCCESS; // Exit the UEFI application
 }
